b711a6f2f5a63be82408df40ad09fee5070d5c38
Pratiksha Choudhury <pratiksha70@gmail.com>
2014-05-02 14:28:14 -0700

    added values

--- src/main/java/thymeleafexamples/gtvg/business/entities/repositories/VendorRepository.java
+++ src/main/java/thymeleafexamples/gtvg/business/entities/repositories/VendorRepository.java
@@ -45,6 +45,28 @@ public class VendorRepository
         prod12.setInStock(true);
         vend1.getProducts().add(prod12);
         
+        final Product prod13 = new Product();
+        prod13.setId(Integer.valueOf(13));
+        prod13.setName("onion");
+        prod13.setPrice(new BigDecimal("0.99"));
+        prod13.setInStock(true);
+        vend1.getProducts().add(prod13);
+        
+        
+        final Product prod14 = new Product();
+        prod14.setId(Integer.valueOf(14));
+        prod14.setName("Frozen Food");
+        prod14.setPrice(new BigDecimal("6.99"));
+        prod14.setInStock(true);
+        vend1.getProducts().add(prod14);
+        
+        final Product prod15 = new Product();
+        prod15.setId(Integer.valueOf(15));
+        prod15.setName("Dairy Product");
+        prod15.setPrice(new BigDecimal("4.99"));
+        prod15.setInStock(true);
+        vend1.getProducts().add(prod15);
+        
         this.vendorsById.put(vend1.getId(), vend1); 
         
         //Vendor2
@@ -54,7 +76,7 @@ public class VendorRepository
         
         final Product prod21 = new Product();
         prod21.setId(Integer.valueOf(21));
-        prod21.setName("thyme");
+        prod21.setName("tomato");
         prod21.setPrice(new BigDecimal("3.25"));
         prod21.setInStock(true);
         vend2.getProducts().add(prod21);
@@ -66,7 +88,29 @@ public class VendorRepository
         prod22.setInStock(true);
         vend2.getProducts().add(prod22);
         
-        this.vendorsById.put(vend2.getId(), vend2);    
+        final Product prod23 = new Product();
+        prod23.setId(Integer.valueOf(23));
+        prod23.setName("basil");
+        prod23.setPrice(new BigDecimal("1.50"));
+        prod23.setInStock(true);
+        vend2.getProducts().add(prod23);
+       
+        
+        final Product prod24 = new Product();
+        prod24.setId(Integer.valueOf(24));
+        prod24.setName("Fresh fruits");
+        prod24.setPrice(new BigDecimal("4.99"));
+        prod24.setInStock(true);
+        vend2.getProducts().add(prod24);
+        
+        final Product prod25 = new Product();
+        prod25.setId(Integer.valueOf(25));
+        prod25.setName("Juices");
+        prod25.setPrice(new BigDecimal("3.25"));
+        prod25.setInStock(true);
+        vend2.getProducts().add(prod25);
+        
+        this.vendorsById.put(vend2.getId(), vend2);   
         
         //Vendor3
         final Vendor vend3 = new Vendor();
@@ -87,6 +131,21 @@ public class VendorRepository
         prod32.setInStock(true);
         vend3.getProducts().add(prod32);
         
+        final Product prod33 = new Product();
+        prod33.setId(Integer.valueOf(33));
+        prod33.setName("tomato");
+        prod33.setPrice(new BigDecimal("2.95"));
+        prod33.setInStock(true);
+        vend3.getProducts().add(prod33);
+       
+        
+        final Product prod34 = new Product();
+        prod34.setId(Integer.valueOf(34));
+        prod34.setName("chicken");
+        prod34.setPrice(new BigDecimal("6.25"));
+        prod34.setInStock(true);
+        vend3.getProducts().add(prod34);
+        
         this.vendorsById.put(vend3.getId(), vend3);    
         
         //Vendor4
@@ -95,7 +154,7 @@ public class VendorRepository
         vend4.setName("Sprouts");
         
         final Product prod41 = new Product();
-        prod41.setId(Integer.valueOf(21));
+        prod41.setId(Integer.valueOf(41));
         prod41.setName("potato");
         prod41.setPrice(new BigDecimal("7.25"));
         prod41.setInStock(true);
@@ -108,6 +167,20 @@ public class VendorRepository
         prod42.setInStock(true);
         vend4.getProducts().add(prod42);
         
+        final Product prod43 = new Product();
+        prod43.setId(Integer.valueOf(43));
+        prod43.setName("tomato");
+        prod43.setPrice(new BigDecimal("8.65"));
+        prod43.setInStock(true);
+        vend4.getProducts().add(prod43);
+        
+        final Product prod44 = new Product();
+        prod44.setId(Integer.valueOf(44));
+        prod44.setName("Sea Food");
+        prod44.setPrice(new BigDecimal("9.25"));
+        prod44.setInStock(true);
+        vend4.getProducts().add(prod44);
+        
         this.vendorsById.put(vend4.getId(), vend4);
         
         //Vendor5
@@ -116,19 +189,39 @@ public class VendorRepository
         vend5.setName("Target");
         
         final Product prod51 = new Product();
-        prod51.setId(Integer.valueOf(21));
+        prod51.setId(Integer.valueOf(51));
         prod51.setName("celery");
         prod51.setPrice(new BigDecimal("13.25"));
         prod51.setInStock(true);
         vend5.getProducts().add(prod51);
         
         final Product prod52 = new Product();
-        prod52.setId(Integer.valueOf(21));
+        prod52.setId(Integer.valueOf(52));
         prod52.setName("potato");
         prod52.setPrice(new BigDecimal("14.25"));
         prod52.setInStock(true);
         vend5.getProducts().add(prod52);
         
+        final Product prod53 = new Product();
+        prod53.setId(Integer.valueOf(53));
+        prod53.setName("basil");
+        prod53.setPrice(new BigDecimal("6.25"));
+        prod53.setInStock(true);
+        vend5.getProducts().add(prod53);
+        
+        final Product prod54 = new Product();
+        prod54.setId(Integer.valueOf(54));
+        prod54.setName("tomato");
+        prod54.setPrice(new BigDecimal("7.25"));
+        prod54.setInStock(true);
+        vend5.getProducts().add(prod54);
+        
+        final Product prod55 = new Product();
+        prod55.setId(Integer.valueOf(55));
+        prod55.setName("cloths");
+        prod55.setPrice(new BigDecimal("18.25"));
+        prod55.setInStock(true);
+        vend5.getProducts().add(prod55);
         this.vendorsById.put(vend5.getId(), vend5);    
         
     }
